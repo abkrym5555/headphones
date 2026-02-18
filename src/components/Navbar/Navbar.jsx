1d164a68b20baba2eeb1da477c6f180815a11a56
abkrym5555 <momo542004@gmail.com>
2026-02-18 14:37:29 +0200

    first commit with nav component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { SlEarphones } from "react-icons/sl";
+import { MdMenu } from "react-icons/md";
+const NabarMenue = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "About",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Content",
+    link: "#",
+  },
+];
+function Navbar() {
+  return (
+    <>
+      <div className="bg-brandDark py-4 text-white">
+        <nav className="container flex items-center justify-between">
+          <div>
+            <a href="#" className="text-xl font-bold uppercase">
+              playing /{" "}
+              <span className="font-extralight text-white/70 uppercase">
+                market
+              </span>
+            </a>
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-4">
+              {NabarMenue.map((nv) => (
+                <li key={nv.id}>
+                  <a
+                    href={`${nv.link}`}
+                    className="inline-block px-3 py-2 text-sm uppercase"
+                  >
+                    {nv.title}
+                  </a>
+                </li>
+              ))}
+              <SlEarphones />
+            </ul>
+          </div>
+          <div>
+            <MdMenu />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
